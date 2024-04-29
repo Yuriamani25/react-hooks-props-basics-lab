bfd33e7ab2da2797cbb36ac605c40db30b00bd83
YuriAmani <yuriamani7@gmail.com>
2024-04-29 12:23:29 +0300

    react-hooks-props-basics-lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Home(props) {
+function Home({name, color, city}) {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h2>test</h2>
+      <h1 style={{ color:color }}>
+        {name} is a Web Developer from {city}
       </h1>
     </div>
   );
